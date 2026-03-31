90c35d30ae7e9c6d606b755ef937e96a95dc1436
Ivan Alpatov <ivan@alpatov.family>
2026-03-31 11:17:19 +0300

    Initial commit

---
+++ EremexAvaloniaEmptyWindowIssue/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Eremex.AvaloniaUI.Controls.Common;
+
+namespace EremexAvaloniaEmptyWindowIssue.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public ICommand OpenFile { get; }
+    public ICommand SaveFile { get; }
+
+    public MainWindowViewModel()
+    {
+        OpenFile = new AsyncRelayCommand<MxWindow>(async (window) =>
+        {
+            if (window == null)
+            {
+                throw new Exception("Window is Null");
+            }
+        });
+        
+        SaveFile = new AsyncRelayCommand<MxWindow>(async (window) =>
+        {
+            if (window == null)
+            {
+                throw new Exception("Window is Null");
+            }
+        });
+    }
+}
---
+++ EremexAvaloniaEmptyWindowIssue/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Eremex.AvaloniaUI.Controls.Common;
+
+namespace EremexAvaloniaEmptyWindowIssue.Views;
+
+public partial class MainWindow : MxWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
